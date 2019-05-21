f5b5ba5c08aaee3b948ff05c375fa864b915a3b1
Sean Carolan <scarolan@hashicorp.com>
2019-05-21 03:55:19 +0000

    Testing this branch.

--- files/deploy_app.sh
+++ files/deploy_app.sh
@@ -4,13 +4,13 @@
 
 cat << EOM > /var/www/html/index.html
 <html>
-  <head><title>Meow!</title></head>
+  <head><title>Woof!</title></head>
   <body>
   <div style="width:800px;margin: 0 auto">
   <center><img src="http://${PLACEHOLDER}/${WIDTH}/${HEIGHT}"></img></center>
-  <center><h2>Meow World!</h2></center>
+  <center><h2>Ruff World!</h2></center>
   <!-- BEGIN -->
-  Welcome to ${PREFIX}'s app. Replace this text with your own. 
+  Welcome to ${PREFIX}'s app. Here's some new improved text!
   <!-- END -->
   </div>
   </body>
